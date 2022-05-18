74d1e7cabefa7607bdff36001ef11f0655687b56
CENSORED <55275098+today2324@users.noreply.github.com>
2022-05-18 09:51:33 +0900

    Create 2020-4.cpp

---
+++ 2020-4.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+typedef struct {
+	char Num;
+	int StationNum;
+	int waiting_time;	
+}BUS;
+BUS Bus;
+
+bool compare(BUS a, BUS b)
+{
+	return a.waiting_time > b.waiting_time;
+}
+
+int main()
+{
+	vector<BUS> Bus;
+	int BlockNum;
+	int information_block;
+	int input_time;
+	int Station;
+	
+	for (size_t i = 0; i < BlockNum; i++)
+	{
+		cin >> Bus[i].Num >> Station;
+		for (size_t j = 0; j < Station; j++)
+		{
+			cin >> Bus[i].StationNum >> Bus[i].waiting_time;
+		}
+	}
+	cin >> input_time;
+
+	sort(Bus.begin(), Bus.end(), compare);
+
+} 
