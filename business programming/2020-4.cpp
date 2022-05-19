76466bd2d6ea2ed3a09a8ed169f7646c6cf2c74f
CENSORED <55275098+today2324@users.noreply.github.com>
2022-05-19 10:57:56 +0900

    Update 2020-4.cpp

--- business programming/2020-4.cpp
+++ business programming/2020-4.cpp
@@ -5,34 +5,41 @@ using namespace std;
 
 typedef struct {
 	char Num;
-	int StationNum;
-	int waiting_time;
+	int Station;
+	int StationNum[100];
+	int waiting_time[100];
 }BUS;
-BUS Bus;
+vector<BUS> Bus;
+int BlockNum;
+int input_time;
 
-bool compare(BUS a, BUS b)
+void bus_search(int i)
 {
-	return a.waiting_time > b.waiting_time;
-}
+	for (size_t k = 0; k < BlockNum; k++)
+	{
+		for (size_t j = 0; j < Bus[k].Station; j++)
+		{
+			if (input_time == Bus[k].StationNum[j])
+			{
 
+			}
+		}
+	}
+}
 int main()
 {
-	vector<BUS> Bus;
-	int BlockNum;
+	BUS temp;
+	int time_temp[100];
 	int information_block;
-	int input_time;
-	int Station;
+	cin >> BlockNum;
 
 	for (size_t i = 0; i < BlockNum; i++)
 	{
-		cin >> Bus[i].Num >> Station;
-		for (size_t j = 0; j < Station; j++)
+		cin >> temp.Num >> temp.Station;
+		for (size_t j = 0; j < temp.Station; j++)
 		{
-			cin >> Bus[i].StationNum >> Bus[i].waiting_time;
+			cin >> Bus[i].StationNum[j] >> Bus[i].waiting_time[j];
 		}
+		Bus.push_back(temp);
 	}
-	cin >> input_time;
-
-	sort(Bus.begin(), Bus.end(), compare);
-
 }
