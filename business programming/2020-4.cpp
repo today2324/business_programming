25e275c95874b94afec710b8b62e0f61a4fd0430
CENSORED <55275098+today2324@users.noreply.github.com>
2022-05-20 18:39:27 +0900

    Create 2020-4.cpp

--- business programming/2020-4.cpp
+++ business programming/2020-4.cpp
@@ -3,43 +3,85 @@
 #include<algorithm>
 using namespace std;
 
-typedef struct {
-	char Num;
-	int Station;
-	int StationNum[100];
-	int waiting_time[100];
+typedef struct BUS {
+	char* Number = new char;
+	int Station = 0;
+	int StationNum[100] = { 0, };
+	int waiting_time[100] = { 0, };
 }BUS;
-vector<BUS> Bus;
-int BlockNum;
-int input_time;
-
-void bus_search(int i)
-{
-	for (size_t k = 0; k < BlockNum; k++)
-	{
-		for (size_t j = 0; j < Bus[k].Station; j++)
-		{
-			if (input_time == Bus[k].StationNum[j])
-			{
-
-			}
-		}
-	}
-}
+vector<BUS> bus;
+BUS temp;
 int main()
 {
-	BUS temp;
-	int time_temp[100];
-	int information_block;
-	cin >> BlockNum;
+	int testcase;
+	int inputTime;
+	bool isdel = false;
+	int count = 0;
+	cin >> testcase;
 
-	for (size_t i = 0; i < BlockNum; i++)
+	for (size_t i = 0; i < testcase; i++)
 	{
-		cin >> temp.Num >> temp.Station;
-		for (size_t j = 0; j < temp.Station; j++)
+		cin >> temp.Number >> temp.Station;
+		for (size_t l = 0; l < temp.Station; l++)
 		{
-			cin >> Bus[i].StationNum[j] >> Bus[i].waiting_time[j];
+			cin >> temp.StationNum[l] >> temp.waiting_time[l];
 		}
-		Bus.push_back(temp);
+		bus.push_back(temp);
 	}
+	cin >> inputTime;
+	for (size_t i = 0; i < testcase; i++)
+	{
+		if (inputTime != bus[i].StationNum[count])
+		{
+			cout << "test" << endl;
+			bus[i].StationNum[count] = bus[i].StationNum[count +1];
+			bus[i].waiting_time[count] = bus[i].waiting_time[count +1];
+			for (size_t l = 0; l < testcase; l++)
+			{
+				cout << bus[l].Number << " " << bus[l].Station << endl;
+				for (size_t k = 0; k < bus[l].Station; k++)
+				{
+					cout << bus[l].StationNum[k] << " " << bus[l].waiting_time[k] << endl;
+				}
+			}
+			cout << "test clear" << endl;
+
+
+
+
+
+
+		}
+		else
+		{
+			isdel = true;
+		}
+
+		if (isdel)
+		{
+			cout << "erase test" << endl;
+			bus.erase(bus.begin() + i);
+			isdel = false;
+			/*for (size_t l = 0; l < testcase; l++)
+			{
+				cout << bus[l].Number << " " << bus[l].Station << endl;
+				for (size_t k = 0; k < bus[l].Station; k++)
+				{
+					cout << bus[l].StationNum[k] << " " << bus[l].waiting_time[k] << endl;
+				}
+			}*/
+			cout << "erase test clear" << endl;
+		}
+	}
+
+	for (size_t i = 0; i < testcase; i++)
+	{
+		cout << bus[i].Number << " " << bus[i].Station << endl;
+		for (size_t k = 0; k < bus[i].Station; k++)
+		{
+			cout << bus[i].StationNum[k] << " " << bus[i].waiting_time[k] << endl;
+		}
+	}
+
+
 }
