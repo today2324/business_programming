16741f4e21f016235f31611126016a008d013819
CENSORED <55275098+today2324@users.noreply.github.com>
2022-05-26 01:20:21 +0900

    Update 2020-5.cpp

--- business programming/2020-5.cpp
+++ business programming/2020-5.cpp
@@ -1,30 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 #define TEN 10
 using namespace std;
 
-int BFS(int n ,int m)
+int decimal=0;
+int c = 1;
+void divisor(int 분자, int 분모)
 {
-
+	if (분자 % 2 && 분모 % 2)
+	{
+		분자 /= 2;
+		분모 /= 2;
+		divisor(분자, 분모);
+	}
+	else if (분자 % 3 && 분모 % 3)
+	{
+		분자 /= 3;
+		분모 /= 3;
+		divisor(분자, 분모);
+	}
+	else if (분자 % 5 && 분모 % 5)
+	{
+		분자 /= 5;
+		분모 /= 5;
+		divisor(분자, 분모);
+	}
+	else if (분자 % 7 && 분모 % 7)
+	{
+		분자 /= 7;
+		분모 /= 7;
+		divisor(분자, 분모);
+	}
+	else
+	{
+		decimal = 분자;
+		c = 분모;
+	}
 }
 
 int main()
 {
-	float temp;
-	float decimal;
-	int c;
-	int num = (int)decimal;
-	cin >> temp;
-	decimal = temp;
+	int zero;
+	int Divisor;
+	scanf("%d.%d", &zero, &decimal);
+
+	cout << decimal << endl;
+	Divisor = decimal;
 	while (decimal)
 	{
-		if (decimal > 1)
-		{
-			decimal -= (int)decimal;
-		}
-		else
-		{
-			decimal *= TEN;
-			c *= TEN;
-		}
+		decimal /= TEN;
+		c *= TEN;
 	}
+
+	divisor(Divisor, c);
+	Divisor = decimal;
+	cout << Divisor << "/" << c << endl;
 }
